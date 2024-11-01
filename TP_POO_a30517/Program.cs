19a0d45e88147b023d188bd18b68e8ecbb498e3f
claudionandes <a30517@alunos.ipca.pt>
2024-11-01 15:20:22 +0000

    Valid Email

--- TP_POO_a30517/Program.cs
+++ TP_POO_a30517/Program.cs
@@ -18,6 +18,7 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 using System.Data;
 using System.Net;
 using System.Xml.Linq;
+using TP_POO_a30517.Services;
 
 
 namespace TP_POO_a30517
@@ -33,23 +34,41 @@ namespace TP_POO_a30517
             //ApplicationConfiguration.Initialize();
             //Application.Run(new Form1());
             #region Person
-            Nurse nurse = new Nurse("Joana Silva Ferreira", new DateOnly(1978, 10, 10), "1234555", "987654321", "teste@teste.pt", "Rua de Viena 40", "Portuguesa", Roles.Enfermeiro, TeamType.INEM, PersonStatus.Disponível, "Joana Silva", "12345", "Pneumologia");
+            PersonsManager personsManager = new PersonsManager();
+            Nurse nurse = new Nurse("Joana Silva Ferreira", new DateOnly(1978, 10, 10), "1234555", "987654321", "testeteste.pt", "Rua de Viena 40", "Portuguesa", Roles.Enfermeiro, TeamType.INEM, PersonStatus.Disponível, "Joana Silva", "12345", "Pneumologia");
             Doctor doctor = new Doctor("Carlos Teixeira Pereira", new DateOnly(1990, 05, 29), "5820288", "912345678", "Email@teste.pt", "Rua Porto, 100", "Portuguesa", Roles.Médico, TeamType.INEM_e_Bombeiros, PersonStatus.Disponível, "Carlos Pereira", "11111", "Cardiologia");
             FireFighter firefighter = new FireFighter("Pedro Sousa", new DateOnly(1980, 04, 30), "456789123", "923456789", "POO@teste.pt", "Rua Campos, 67", "Portuguesa", Roles.Bombeiro, TeamType.Bombeiros, PersonStatus.Disponível, 5, "Resgate em Incêndios");
             EmergencyTechnician technician = new EmergencyTechnician("André Fernandes", new DateOnly(1985, 10, 15), "789456123", "924567890", "tec@teste.pt", "Rua Lisboa, 23", "Portuguesa", Roles.TécnicoEmergência, TeamType.INEM,PersonStatus.Disponível, "André F.", "22222");
+
+            // Adicionando profissionais ao gerenciador
+            try
+            {
+                personsManager.AddPerson(nurse);
+                personsManager.AddPerson(doctor);
+                personsManager.AddPerson(firefighter);
+                personsManager.AddPerson(technician);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
             #endregion
 
             #region Professional Details
-                //Console.WriteLine("Professional Details:");
-                //Console.WriteLine(nurse.ReturnsValuesPerson());
-                //Console.WriteLine();
-                //Console.WriteLine(doctor.ReturnsValuesPerson());
-                //Console.WriteLine();
-                //Console.WriteLine(firefighter.ReturnsValuesPerson());
-                #endregion
+            //Console.WriteLine("Professional Details:");
+            //Console.WriteLine(nurse.ReturnsValuesPerson());
+            //Console.WriteLine();
+            //Console.WriteLine(doctor.ReturnsValuesPerson());
+            //Console.WriteLine();
+            //Console.WriteLine(firefighter.ReturnsValuesPerson());
+            #endregion
 
             #region INEM Team Creation
-                INEM inemTeam = new INEM("INEM Equipa A", doctor, nurse, technician, VehiclesType.Ambulância_de_Emergência_Médica_AEM, EquipmentType.Desfibrilhador, TeamStatus.Alocado);
+            INEM inemTeam = new INEM("INEM Equipa A", doctor, nurse, technician, VehiclesType.Ambulância_de_Emergência_Médica_AEM, EquipmentType.Desfibrilhador, TeamStatus.Alocado);
             #endregion
 
             #region Display INEM Team Details
